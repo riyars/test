d091872d61c9f685c48d6a627a07a6de2c90f783
riyars <riyarachelsamuel@cs.ajce.in>
2017-10-24 18:01:18 +0530

    Second Commit

--- mlone.py
+++ mlone.py
@@ -1,4 +1,4 @@
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_iris
 
 iris=load_iris()
@@ -9,7 +9,7 @@ labels=iris.target
 from sklearn.cross_validation import train_test_split
 X_train,X_test,Y_train,Y_test = train_test_split(features,labels,test_size=.3)
 
-clf=KNeighborsClassifier()
+clf=DecisionTreeClassifier()
 clf.fit(X_train,Y_train)
 
 p=clf.predict(X_test)
